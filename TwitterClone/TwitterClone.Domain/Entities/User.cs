b388c8e826805e59167e6f7d8373755e314397dc
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-08-08 15:17:46 +0600

    Add User class

---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Domain.Entities
+{
+    internal class User
+    {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+        public User()
+        {
+            _id = Guid.NewGuid();
+        }
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+    }
+}
