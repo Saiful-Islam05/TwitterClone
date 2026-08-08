b002665ee840e8cc075a16bd3927720e8d36692a
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-08-08 21:18:12 +0600

    Add Tweet,Like And Retweet Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Retweet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Domain.Entities
+{
+    public class Retweet
+    {
+        private Guid _id;
+        private Guid _userId;
+        private Guid _tweetId;
+        private DateTime _retweetedAt;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+        }
+
+        public DateTime RetweetedAt
+        {
+            get { return _retweetedAt; }
+        }
+    }
+}
