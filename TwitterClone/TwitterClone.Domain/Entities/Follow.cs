1f59d86983fb75f11deebb5873c151f2c48c0d42
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-08-08 21:28:42 +0600

    Add Follow.cs Class

--- TwitterClone/TwitterClone.Domain/Entities/Follow.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Follow.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace TwitterClone.Domain.Entities
 {
-    internal class Follow
+    public class Follow
     {
+        private Guid _id;
+        private Guid _followerId;
+        private Guid _followingId;
+        private DateTime _followedAt;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid FollowerId
+        {
+            get { return _followerId; }
+        }
+
+        public Guid FollowingId
+        {
+            get { return _followingId; }
+        }
+
+        public DateTime FollowedAt
+        {
+            get { return _followedAt; }
+        }
     }
 }
