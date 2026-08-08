43adba240b72fbfb8e00141a76375ea9221d834c
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-08-08 15:38:32 +0600

    Add Comments

--- TwitterClone/TwitterClone.Domain/Entities/User.cs
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -8,25 +8,33 @@ namespace TwitterClone.Domain.Entities
 {
     internal class User
     {
+        // Unique identifier for the user
         private Guid _id;
+
+        //Private fields for user properties
         private string _firstName;
         private string _lastName;
         private string _email;
 
+        // Constructor to initialize the user with a unique identifier
         public User()
         {
             _id = Guid.NewGuid();
         }
+
+        // Public properties to access user information
 
         public Guid Id
         {
             get { return _id; }
         }
 
+        // Public property for the user's first name
         public string FirstName
         {
             get { return _firstName; }
             set { _firstName = value; }
         }
+
     }
 }
