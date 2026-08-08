1502ee99f4a4678aab60f83b81bbbe87ef238bec
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-08-08 22:33:18 +0600

    Assignment 06 Completed

--- TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
@@ -8,5 +8,29 @@ namespace TwitterClone.Domain.Entities
 {
     internal class Bookmark
     {
+        private Guid _id;
+        private Guid _userId;
+        private Guid _tweetId;
+        private DateTime _bookmarkedAt;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+        }
+
+        public DateTime BookmarkedAt
+        {
+            get { return _bookmarkedAt; }
+        }
     }
 }
