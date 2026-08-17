41320b80004d192b7a4ffcf5946fde9c7e77200d
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-08-17 11:28:55 +0600

    Assignment 09:AbstractClass And AbstractMethod

---
+++ TwitterClone/TwitterClone.Domain/Entities/Bookmark.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Domain.Entities
+{
+    internal class Bookmark
+    {
+        private Guid _id;
+        private Guid _userId;
+        private Guid _tweetId;
+        private DateTime _bookmarkedAt;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public Guid TweetId
+        {
+            get { return _tweetId; }
+        }
+
+        public DateTime BookmarkedAt
+        {
+            get { return _bookmarkedAt; }
+        }
+    }
+}
--- TwitterClone/TwitterClone.Domain/Entities/Notification.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Notification.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace TwitterClone.Domain.Entities
 {
+<<<<<<< HEAD
     public abstract class Notification : BaseEntity
     {
        
@@ -33,12 +34,54 @@ namespace TwitterClone.Domain.Entities
             get { return _message; }
             set { _message = value; }
         }
+=======
+    public class Notification
+    {
+        private Guid _id;
+        private Guid _recipientId;
+        private Guid _senderId;
+        private string _type;
+        private string _content;
+        private bool _isRead;
+        private DateTime _createdAt;
+
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid RecipientId
+        {
+            get { return _recipientId; }
+        }
+
+        public Guid SenderId
+        {
+            get { return _senderId; }
+        }
+
+        public string Type
+        {
+            get { return _type; }
+        }
+
+        //If Content need.So we can have a setter for it
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        //It will change after read so we need to have a setter for it
+>>>>>>> e8c22baae033aff7016f7940437a69c312b6baaa
         public bool IsRead
         {
             get { return _isRead; }
             set { _isRead = value; }
         }
 
+<<<<<<< HEAD
         public override string DescribeRecord()
         {
             var baseRecord = base.DescribeRecord();
@@ -53,5 +96,12 @@ namespace TwitterClone.Domain.Entities
         */
         public abstract string GetMessage();
      
+=======
+        public DateTime CreatedAt
+        {
+            get { return _createdAt; }
+        }
+
+>>>>>>> e8c22baae033aff7016f7940437a69c312b6baaa
     }
 }
