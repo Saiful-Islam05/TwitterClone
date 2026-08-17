d757acd3b07f6fece7d3e6f0f52b9aa28f89cc59
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-08-17 07:35:50 +0600

    Resolve merge conflicts - keep all notification files

---
+++ TwitterClone/TwitterClone.Domain/Entities/SystemNotification.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Domain.Entities
+{
+    public class SystemNotification : Notification
+    {
+        public SystemNotification() : base("System")
+        {
+            
+        }
+
+        public void AddMessage(string message)
+        {
+            Message = message;
+        }
+
+        public override string DescribeRecord()
+        {
+            return base.DescribeRecord();
+        }
+
+        public override string GetMessage()
+        {
+            return $"System Notification: {Message}";
+        }
+
+
+    }
+}
