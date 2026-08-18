dc3e7a897c147733d815a469cb521283dd68eda4
Saiful-Islam05 <saifulislames5112@gmail.com>
2026-08-18 09:01:52 +0600

    Class-10-Assignment Implement IFollowable, ILikable, INotifiable interfaces

---
+++ TwitterClone/TwitterClone.Domain/Entities/IFollowable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterClone.Domain.Entities
+{
+    public interface IFollowable
+    {
+        void Follow(Guid userId);
+        void Unfollow(Guid userId);
+    }
+}
